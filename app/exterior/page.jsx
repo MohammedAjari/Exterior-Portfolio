2d806e645b32f1df086f373139429523a0e48309
MohammedAjari <ajarimohammed0987@gmail.com>
2023-10-16 23:29:29 +0530

    optimized mobile responsive

--- app/exterior/page.jsx
+++ app/exterior/page.jsx
@@ -27,11 +27,11 @@ export default function page() {
         },
     ];
     return (
-        <div className="">
+        <div className="overflow-hidden">
             <div className="bg-[url('/4.jpg')] bg-center bg-cover ">
-                <h1 className="container py-64 text-6xl font-semibold tracking-widest text-white " style={{ fontFamily: 'Montserrat, sans-serif' }}>EXTERIOR PROJECTS</h1>
+                <h1 className="container py-64 text-5xl lg:text-6xl font-semibold tracking-widest text-white " style={{ fontFamily: 'Montserrat, sans-serif' }}>EXTERIOR PROJECTS</h1>
             </div>
-            <div className="container grid grid-cols-2 gap-8 py-8">
+            <div className="container grid grid-cols-1 lg:grid-cols-2 gap-8 py-8">
 
                 {projects.map((project) => (
                     <div key={project.id} className="relative overflow-hidden rounded-xl group">
@@ -39,9 +39,9 @@ export default function page() {
                             <Image src={project.image} width={480} height={380} alt="" className="w-full" />
                         </div>
                         <div className="absolute bottom-0 flex-col items-center justify-end w-full gap-32 lg:p-12 
-                    lg:text-xl text-xs text-white transition duration-300 ease-in-out translate-y-full bg-gradient-to-b from-transparent to-black group-hover:translate-y-0">
-                            <h1 className="text-xs lg:text-2xl font-semibold" style={{ fontFamily: 'Montserrat, sans-serif' }}>{project.name}</h1>
-                            <p className=" lg:py-4 text-xs " style={{}}>{project.description}</p>
+                    text-xl p-3 text-white transition duration-300 ease-in-out translate-y-full bg-gradient-to-b from-transparent to-black group-hover:translate-y-0">
+                            <h1 className="text-2xl font-semibold" style={{ fontFamily: 'Montserrat, sans-serif' }}>{project.name}</h1>
+                            <p className=" lg:py-4  " style={{}}>{project.description}</p>
                         </div>
                     </div>
 
